56a92d761b37ebec4b5d65b5e8be88e0c6ecbea3
Sondos-Mahmoud <115264160+Sondos-Mahmoud@users.noreply.github.com>
2024-04-28 01:36:27 +0200

    Add files via upload

---
+++ src/pages/Addpatient.jsx
@@ -0,0 +1,273 @@
+export default function AddPatient() {
+    return (
+      <>
+        <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+          <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+            <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+              Add Patient
+            </h2>
+          </div>
+  
+          <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+            <form className="space-y-6" action="#" method="POST">
+              <div>
+                <label
+                  htmlFor="name"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Name
+                </label>
+                <div className="mt-2">
+                  <input
+                    id="name"
+                    name="name"
+                    type="text"
+                    autoComplete="name"
+                    required
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+  
+              <div>
+                <label
+                  htmlFor="email"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Email
+                </label>
+                <div className="mt-2">
+                  <input
+                    id="email"
+                    name="email"
+                    type="email"
+                    autoComplete="email"
+                    required
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+  
+              <div>
+                <label
+                  htmlFor="age"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Age
+                </label>
+                <div className="mt-2">
+                  <input
+                    id="age"
+                    name="age"
+                    type="number"
+                    autoComplete="age"
+                    required
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+  
+              <div>
+                <label
+                  htmlFor="gender"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Gender
+                </label>
+                <div className="mt-2">
+                  <select
+                    id="gender"
+                    name="gender"
+                    autoComplete="gender"
+                    required
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  >
+                    <option value="male">Male</option>
+                    <option value="female">Female</option>
+                    <option value="other">Other</option>
+                  </select>
+                </div>
+              </div>
+  
+              <div>
+                <label
+                  htmlFor="mobile"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Mobile
+                </label>
+                <div className="mt-2">
+                  <input
+                    id="mobile"
+                    name="mobile"
+                    type="tel"
+                    autoComplete="mobile"
+                    required
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+  
+              <div>
+                <label
+                  htmlFor="visitDate"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Visit Date
+                </label>
+                <div className="mt-2">
+                  <input
+                    id="visitDate"
+                    name="visitDate"
+                    type="date"
+                    autoComplete="visit-date"
+                    required
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+  
+              <div>
+                <label
+                  htmlFor="diagnosis"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Diagnosis
+                </label>
+                <div className="mt-2">
+                  <textarea
+                    id="diagnosis"
+                    name="diagnosis"
+                    autoComplete="diagnosis"
+                    required
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  ></textarea>
+                </div>
+              </div>
+  
+              <div>
+                <label
+                  htmlFor="medicine"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Medicine
+                </label>
+                <div className="mt-2">
+                  <input
+                    id="medicine"
+                    name="medicine"
+                    type="text"
+                    autoComplete="medicine"
+                    required
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+  
+              <div>
+                <label
+                  htmlFor="dosage"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Dosage
+                </label>
+                <div className="mt-2">
+                  <input
+                    id="dosage"
+                    name="dosage"
+                    type="text"
+                    autoComplete="dosage"
+                    required
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+  
+              <div>
+                <label
+                  htmlFor="frequency"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Frequency per day
+                </label>
+                <div className="mt-2">
+                  <input
+                    id="frequency"
+                    name="frequency"
+                    type="text"
+                    autoComplete="frequency"
+                    required
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+  
+              <div>
+                <label
+                  htmlFor="tests"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Tests
+                </label>
+                <div className="mt-2">
+                  <textarea
+                    id="tests"
+                    name="tests"
+                    autoComplete="tests"
+                    required
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  ></textarea>
+                </div>
+              </div>
+  
+              <div>
+                <label
+                  htmlFor="treatment"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Treatment
+                </label>
+                <div className="mt-2">
+                  <textarea
+                    id="treatment"
+                    name="treatment"
+                    autoComplete="treatment"
+                    required
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  ></textarea>
+                </div>
+              </div>
+  
+              <div>
+                <label
+                  htmlFor="medicalHistory"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Medical History
+                </label>
+                <div className="mt-2">
+                  <textarea
+                    id="medicalHistory"
+                    name="medicalHistory"
+                    autoComplete="medical-history"
+                    required
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  ></textarea>
+                </div>
+              </div>
+  
+              <div>
+                <button
+                  type="submit"
+                  className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                >
+                  Add Patient
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </>
+    );
+  }
+  

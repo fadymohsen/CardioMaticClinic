d0692e96946aaee77f8cb81da1b016b1e85cfb6b
Sondos-Mahmoud <115264160+Sondos-Mahmoud@users.noreply.github.com>
2024-04-28 16:04:42 +0200

    Add files via upload

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -38,9 +38,9 @@ export default function Contact() {
         />
       </div>
       <div className="mx-auto max-w-2xl text-center">
-        <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Contact sales</h2>
+        <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Contact us</h2>
         <p className="mt-2 text-lg leading-8 text-gray-600">
-          Aute magna irure deserunt veniam aliqua magna enim voluptate.
+          Have Any Query? pleaseContact Us!
         </p>
       </div>
       <form action="#" method="POST" className="mx-auto mt-16 max-w-xl sm:mt-20">
@@ -181,7 +181,7 @@ export default function Contact() {
             type="submit"
             className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
           >
-            Lets talk
+            Send message
           </button>
         </div>
       </form>

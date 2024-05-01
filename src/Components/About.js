6284137d58264b373942a1151f8c5b2d837f2ce9
Sondos-Mahmoud <115264160+Sondos-Mahmoud@users.noreply.github.com>
2024-05-01 22:18:00 +0200

    Add files via upload

--- src/Components/About.js
+++ src/Components/About.js
@@ -12,56 +12,29 @@ function About() {
 
       <div className="about-text-content">
         <h3 className="about-title">
-          <span>About Us</span>
+          <span>Why You Should Trust Us? Get To Know About Us!</span>
         </h3>
         <p className="about-description">
-          At CardioMatic Clinic, we understand that entrusting your
-          cardiovascular health to a medical institution requires confidence
-          and assurance.
+        At CardioMatic Clinic, we understand that entrusting your cardiovascular health to a medical institution requires confidence and assurance.
+        With a team of experienced cardiologists at the forefront of cardiac care, cutting-edge technology ensuring accurate diagnoses and interventions, compassionate support throughout your journey, and a commitment to excellence in every aspect of your health and well-being, CardioMatic Clinic stands as your trusted partner in heart health.
         </p>
-
-        <h4 className="about-text-title">Why You Should Trust Us?</h4>
-
-        <div className="about-list">
-          <p className="about-list-item">
-            <span className="about-list-icon">&#10004;</span> Expertise: With a
-            team of experienced cardiologists at the forefront of cardiac care
-            and cutting-edge technology ensuring accurate diagnoses and
-            interventions, we stand as your trusted partner in heart health.
-          </p>
-          <p className="about-list-item">
-            <span className="about-list-icon">&#10004;</span> Compassionate
-            Care: We provide compassionate support throughout your journey,
-            ensuring you feel understood and cared for every step of the way.
-          </p>
-          <p className="about-list-item">
-            <span className="about-list-icon">&#10004;</span> Comprehensive
-            Approach: Our commitment to a comprehensive approach means we
-            consider every aspect of your health and well-being, tailoring
-            treatment plans to suit your individual needs.
-          </p>
-          <p className="about-list-item">
-            <span className="about-list-icon">&#10004;</span> Commitment to
-            Excellence: We strive for excellence in every aspect of our
-            services, ensuring you receive the highest quality care possible.
-          </p>
-        </div>
 
         <h4 className="about-text-title">Your Solutions</h4>
 
         <SolutionStep
-          title="Choose a Specialist"
-          description="Find your perfect specialist and book with ease at Health Plus. Expert doctors prioritize your health, offering tailored care."
+          title="Expertise"
+  
         />
 
         <SolutionStep
-          title="Make a Schedule"
-          description="Choose the date and time that suits you best, and let our dedicated team of medical professionals ensure your well-being with personalized care."
+          title="Compassionate Care "
         />
 
         <SolutionStep
-          title="Get Your Solutions"
-          description="Our experienced doctors and specialists are here to provide expert advice and personalized treatment plans, helping you achieve your best possible health."
+          title="Comprehensive Approach"
+        />
+         <SolutionStep
+          title="Commitment to Excellence"
         />
       </div>
     </div>

9c4c8167e72bec2f9b86474c7d90e3db590da003
Sondos-Mahmoud <115264160+Sondos-Mahmoud@users.noreply.github.com>
2024-05-01 22:40:57 +0200

    Add files via upload

--- src/Components/Doctors.js
+++ src/Components/Doctors.js
@@ -11,43 +11,32 @@ function Doctors() {
     <div className="doctor-section" id="doctors">
       <div className="dt-title-content">
         <h3 className="dt-title">
-          <span>Meet Our Doctors</span>
+          <span>Our Experience Doctors</span>
         </h3>
-
-        <p className="dt-description">
-          Meet our exceptional team of specialist doctors, dedicated to
-          providing top-notch healthcare services at Health Plus. Trust in their
-          knowledge and experience to lead you towards a healthier and happier
-          life.
-        </p>
       </div>
 
       <div className="dt-cards-content">
         <DoctorCard
           img={profile1}
-          name="Dr. Kathryn Murphy"
-          title="General Surgeons"
+          name="Dr. Amira"
           stars="4.9"
           reviews="1800"
         />
         <DoctorCard
           img={profile2}
-          name="Dr. Jacob Jones"
-          title="Hematologists"
+          name="Dr. Ahmed"
           stars="4.8"
           reviews="700"
         />
         <DoctorCard
           img={profile3}
-          name="Dr. Jenny Wilson"
-          title="Endocrinologists"
+          name="Dr. rana"
           stars="4.7"
           reviews="450"
         />
         <DoctorCard
           img={profile4}
-          name="Dr. Albert Flores"
-          title="Hematologists"
+          name="Dr.Omar"
           stars="4.8"
           reviews="500"
         />

f9ca7789f09370dfbe906ee59707f37fcd727185
Sondos-Mahmoud <115264160+Sondos-Mahmoud@users.noreply.github.com>
2024-05-01 22:06:10 +0200

    Add files via upload

--- src/Components/BookAppointment.js
+++ src/Components/BookAppointment.js
@@ -3,6 +3,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircleCheck, faCalendarCheck } from "@fortawesome/free-solid-svg-icons";
 import { useNavigate } from "react-router-dom";
 import "../Styles/BookAppointment.css";
+import DoctorImage from "../Assets/doctor-book-appointment.png"; // Import the image
 
 function BookAppointment() {
   const navigate = useNavigate();
@@ -13,6 +14,9 @@ function BookAppointment() {
 
   return (
     <div className="ba-section">
+      <div className="ba-image-content">
+        <img src={DoctorImage} alt="Doctor Group" className="ba-image1" />
+      </div>
       <div className="ba-text-content">
         <h3 className="ba-title">
           <span>Why Choose Us</span>

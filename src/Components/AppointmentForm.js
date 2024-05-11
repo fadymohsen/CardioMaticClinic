c62cd15d5118562272915db7521adef2df2efdb9
Sondos-Mahmoud <115264160+Sondos-Mahmoud@users.noreply.github.com>
2024-05-11 19:46:26 +0200

    Add files via upload

--- src/Components/AppointmentForm.js
+++ src/Components/AppointmentForm.js
@@ -8,11 +8,9 @@ function AppointmentForm() {
     window.scrollTo({ top: 0, behavior: "smooth" });
   });
 
-  const [patientName, setPatientName] = useState("");
-  const [patientNumber, setPatientNumber] = useState("");
-  const [patientGender, setPatientGender] = useState("default");
-  const [appointmentTime, setAppointmentTime] = useState("");
-  const [preferredMode, setPreferredMode] = useState("default");
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [selectedSlot, setSelectedSlot] = useState("default");
   const [isSubmitted, setIsSubmitted] = useState(false);
   const [formErrors, setFormErrors] = useState({});
 
@@ -21,32 +19,18 @@ function AppointmentForm() {
 
     // Validate form inputs
     const errors = {};
-    if (!patientName.trim()) {
-      errors.patientName = "Patient name is required";
-    } else if (patientName.trim().length < 8) {
-      errors.patientName = "Patient name must be at least 8 characters";
+    if (!fullName.trim()) {
+      errors.fullName = "Full name is required";
     }
 
-    if (!patientNumber.trim()) {
-      errors.patientNumber = "Patient phone number is required";
-    } else if (patientNumber.trim().length !== 10) {
-      errors.patientNumber = "Patient phone number must be of 10 digits";
+    if (!email.trim()) {
+      errors.email = "Email is required";
+    } else if (!/\S+@\S+\.\S+/.test(email)) {
+      errors.email = "Invalid email format";
     }
 
-    if (patientGender === "default") {
-      errors.patientGender = "Please select patient gender";
-    }
-    if (!appointmentTime) {
-      errors.appointmentTime = "Appointment time is required";
-    } else {
-      const selectedTime = new Date(appointmentTime).getTime();
-      const currentTime = new Date().getTime();
-      if (selectedTime <= currentTime) {
-        errors.appointmentTime = "Please select a future appointment time";
-      }
-    }
-    if (preferredMode === "default") {
-      errors.preferredMode = "Please select preferred mode";
+    if (selectedSlot === "default") {
+      errors.selectedSlot = "Please select an available slot";
     }
 
     if (Object.keys(errors).length > 0) {
@@ -55,14 +39,12 @@ function AppointmentForm() {
     }
 
     // Reset form fields and errors after successful submission
-    setPatientName("");
-    setPatientNumber("");
-    setPatientGender("default");
-    setAppointmentTime("");
-    setPreferredMode("default");
+    setFullName("");
+    setEmail("");
+    setSelectedSlot("default");
     setFormErrors({});
 
-    toast.success("Appointment Scheduled !", {
+    toast.success("Appointment Scheduled!", {
       position: toast.POSITION.TOP_CENTER,
       onOpen: () => setIsSubmitted(true),
       onClose: () => setIsSubmitted(false),
@@ -81,61 +63,45 @@ function AppointmentForm() {
 
           <form className="form-content" onSubmit={handleSubmit}>
             <label>
-              Patient Full Name:
+              Full Name:
               <input
                 type="text"
-                value={patientName}
-                onChange={(e) => setPatientName(e.target.value)}
+                value={fullName}
+                onChange={(e) => setFullName(e.target.value)}
                 required
               />
-              {formErrors.patientName && (
-                <p className="error-message">{formErrors.patientName}</p>
+              {formErrors.fullName && (
+                <p className="error-message">{formErrors.fullName}</p>
               )}
             </label>
 
             <br />
             <label>
-              Patient Phone Number:
+              Email:
               <input
-                type="text"
-                value={patientNumber}
-                onChange={(e) => setPatientNumber(e.target.value)}
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
                 required
               />
-              {formErrors.patientNumber && (
-                <p className="error-message">{formErrors.patientNumber}</p>
+              {formErrors.email && (
+                <p className="error-message">{formErrors.email}</p>
               )}
             </label>
 
             <br />
             <label>
-              Patient Gender:
+              Available Slots:
               <select
-                value={patientGender}
-                onChange={(e) => setPatientGender(e.target.value)}
+                value={selectedSlot}
+                onChange={(e) => setSelectedSlot(e.target.value)}
                 required
               >
                 <option value="default">Select</option>
-                <option value="male">Male</option>
-                <option value="female">Female</option>
-                <option value="private">I will inform Doctor only</option>
+                
               </select>
-              {formErrors.patientGender && (
-                <p className="error-message">{formErrors.patientGender}</p>
-              )}
-            </label>
-
-            <br />
-            <label>
-              Preferred Appointment Time:
-              <input
-                type="datetime-local"
-                value={appointmentTime}
-                onChange={(e) => setAppointmentTime(e.target.value)}
-                required
-              />
-              {formErrors.appointmentTime && (
-                <p className="error-message">{formErrors.appointmentTime}</p>
+              {formErrors.selectedSlot && (
+                <p className="error-message">{formErrors.selectedSlot}</p>
               )}
             </label>
 
@@ -150,8 +116,7 @@ function AppointmentForm() {
               className="success-message"
               style={{ display: isSubmitted ? "block" : "none" }}
             >
-              Appointment details has been sent to the patients phone number via
-              SMS.
+              Appointment details have been sent to the patient's email.
             </p>
           </form>
         </div>

f751d3ca19bb7515b0c8e0ad11fb3711f7754072
mohamed mosilhy <93820559+mohamedmosilhy@users.noreply.github.com>
2024-05-12 01:27:04 +0300

    Update Dashboard.js

--- src/Pages/Dashboard.js
+++ src/Pages/Dashboard.js
@@ -1,6 +1,12 @@
 import { Sidebar } from "../Components/Sidebar";
 import { PieChart } from "@mui/x-charts/PieChart";
+import { BarChart } from "@mui/x-charts/BarChart";
+import { LineChart } from "@mui/x-charts/LineChart";
 export default function Dashboard() {
+  const chartsParams = {
+    margin: { bottom: 20, left: 25, right: 5 },
+    height: 300,
+  };
   return (
     <div className="flex">
       <Sidebar tab_index={0} />
@@ -41,73 +47,75 @@ export default function Dashboard() {
               </div>
               <div className=" col-span-3 px-8 pt-8 pb-4 bg-white rounded-lg shadow-md flex flex-col">
                 <h3 className="font-medium leading-6" id="modal-title">
-                  Male vs Female Subjects
+                  Subjects' Age Distribution
                 </h3>
 
                 <p className="mt-2 text-sm ">
-                  See and compare how many male and female subjects have been
-                  scanned by your lab
+                  Figure out how many scans were made for each age category.
                 </p>
                 <div className="mx-auto w-full h-80">
-                  <PieChart
-                    series={[
+                  <BarChart
+                    xAxis={[
                       {
-                        data: [
-                          { id: 0, value: 10, label: "Male" },
-                          { id: 1, value: 15, label: "Female" },
-                        ],
+                        scaleType: "band",
+                        data: ["group A", "group B", "group C"],
                       },
                     ]}
-                    width={400}
-                    height={200}
+                    series={[
+                      { data: [4, 3, 5] },
+                      { data: [1, 6, 3] },
+                      { data: [2, 5, 6] },
+                    ]}
+                    width={500}
+                    height={300}
                   />
                 </div>
               </div>
               <div className=" col-span-3 px-8 pt-8 pb-4 bg-white rounded-lg shadow-md flex flex-col">
                 <h3 className="font-medium leading-6" id="modal-title">
-                  Male vs Female Subjects
+                  Scan Types Distribution
                 </h3>
 
                 <p className="mt-2 text-sm ">
-                  See and compare how many male and female subjects have been
-                  scanned by your lab
+                  Figure out how many scans were made for each scan type.
                 </p>
                 <div className="mx-auto w-full h-80">
-                  <PieChart
-                    series={[
+                  <BarChart
+                    xAxis={[
                       {
-                        data: [
-                          { id: 0, value: 10, label: "Male" },
-                          { id: 1, value: 15, label: "Female" },
-                        ],
+                        scaleType: "band",
+                        data: ["group A", "group B", "group C"],
                       },
                     ]}
-                    width={400}
-                    height={200}
+                    series={[
+                      { data: [4, 3, 5] },
+                      { data: [1, 6, 3] },
+                      { data: [2, 5, 6] },
+                    ]}
+                    width={500}
+                    height={300}
                   />
                 </div>
               </div>
               <div className=" col-span-3 px-8 pt-8 pb-4 bg-white rounded-lg shadow-md flex flex-col">
                 <h3 className="font-medium leading-6" id="modal-title">
-                  Male vs Female Subjects
+                  Number of Scans Over The Year
                 </h3>
 
                 <p className="mt-2 text-sm ">
-                  See and compare how many male and female subjects have been
-                  scanned by your lab
+                  Plotting the number of scans made in each month of the current
+                  year
                 </p>
                 <div className="mx-auto w-full h-80">
-                  <PieChart
+                  <LineChart
+                    {...chartsParams}
                     series={[
                       {
-                        data: [
-                          { id: 0, value: 10, label: "Male" },
-                          { id: 1, value: 15, label: "Female" },
-                        ],
+                        data: [15, 23, 18, 19, 13],
+                        label: "Example",
+                        color: "#4e79a7",
                       },
                     ]}
-                    width={400}
-                    height={200}
                   />
                 </div>
               </div>

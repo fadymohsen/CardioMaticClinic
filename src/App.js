c713cb806667bbc63a5837ee1e90ff06e36ee838
mohamed mosilhy <93820559+mohamedmosilhy@users.noreply.github.com>
2024-05-11 21:18:57 +0300

    changes

--- src/App.js
+++ src/App.js
@@ -3,11 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./Pages/Home";
 import NotFound from "./Pages/NotFound";
-import Appointment from "./Pages/Appointment";
 import Login from "./Pages/Login";
 import SignUp from "./Pages/Signup";
-import AddPatient from "./Pages/Addpatient";
-import PatientRecord from "./Pages/PatientRecord";
 import Dashboard from "./Pages/Dashboard";
 
 function App() {
@@ -18,11 +15,10 @@ function App() {
           <Route index element={<Home />} />
           <Route path="login" element={<Login />} />
           <Route path="signup" element={<SignUp />} />
-          <Route path="dashboard" element={<Dashboard />} />
-          <Route path="appointment" element={<Appointment />} />
+          <Route path="dashboard" element={<Dashboard />}>
+            <Route></Route>
+          </Route>
           <Route path="*" element={<NotFound />} />
-          <Route path="add-patient" element={<AddPatient />} />
-          <Route path="patient-record" element={<PatientRecord />} />
         </Routes>
       </Router>
     </div>
--- src/Pages/Dashboard.js
+++ src/Pages/Dashboard.js
@@ -1,3 +1,5 @@
+import { Sidebar } from "../Components/Sidebar";
+
 export default function Dashboard() {
-  return <div>Dashboard</div>;
+  return <Sidebar></Sidebar>;
 }

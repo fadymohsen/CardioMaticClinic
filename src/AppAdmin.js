5726e16f128d20dc56b98ae6e2d4fb8d13383395
Sondos-Mahmoud <115264160+Sondos-Mahmoud@users.noreply.github.com>
2024-05-14 17:59:57 +0200

    Add files via upload

---
+++ src/AppAdmin.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./Pages/Home";
+import NotFound from "./Pages/NotFound";
+import Login from "./Pages/Login";
+import SignUp from "./Pages/Signup";
+import Dashboard from "./Pages/Dashboard";
+import DoctorsListPage from "./Pages/Doctorlist";
+import PatientListPage from "./Pages/Patientlist";
+import AppointmentsListPage from "./Pages/Appointmentlist";
+
+
+
+
+
+
+function AppAdmin() {
+  return (
+    <div className="App">
+      <Router basename="/">
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="signup" element={<SignUp />} />
+          <Route path="dashboardAdmin" element={<Dashboard />}></Route>
+          <Route path="ViewPatient" element={<PatientListPage />}></Route>
+          <Route path="ViewDoctor" element={<DoctorsListPage/>}></Route>
+          <Route path="ViewAppointments" element={<AppointmentsListPage/>}></Route>
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default AppAdmin;

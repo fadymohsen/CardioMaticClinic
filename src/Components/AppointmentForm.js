215957d608c1a1ff9ee033c2d1eeda6680cb6a1f
mohamed mosilhy <93820559+mohamedmosilhy@users.noreply.github.com>
2024-05-15 04:07:46 +0300

    changes

--- src/Components/AppointmentForm.js
+++ src/Components/AppointmentForm.js
@@ -1,12 +1,11 @@
 import React, { useState, useEffect } from "react";
-import "../Styles/AppointmentForm.css";
 import { ToastContainer, toast } from "react-toastify";
-import Navbar from "./Navbar";
+import "react-toastify/dist/ReactToastify.css";
 
-function AppointmentForm() {
+export default function AppointmentForm() {
   useEffect(() => {
     window.scrollTo({ top: 0, behavior: "smooth" });
-  });
+  }, []);
 
   const [fullName, setFullName] = useState("");
   const [email, setEmail] = useState("");
@@ -52,76 +51,92 @@ function AppointmentForm() {
   };
 
   return (
-    <div className="appointment-form-section">
-      <div className="flex justify-center align-middle">
-        <div className="form-container w-5/12 ">
-          <h2 className="form-title">
-            <span>Book Appointment</span>
-          </h2>
-
-          <form className="form-content" onSubmit={handleSubmit}>
-            <label>
-              Full Name:
+    <div className="appointment-form-section flex mt-20 justify-center ">
+      <div className="form-container w-5/12 p-8 border border-gray-300 rounded-lg">
+        <h2 className="text-xl font-semibold mb-4">Book Appointment</h2>
+        <form onSubmit={handleSubmit} className="space-y-4">
+          <div className="grid md:grid-cols-2 md:gap-6">
+            <div className="relative z-0 w-full mb-5 group">
               <input
                 type="text"
+                name="fullName"
                 value={fullName}
                 onChange={(e) => setFullName(e.target.value)}
+                id="fullName"
+                className={`block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer ${
+                  formErrors.fullName ? "border-red-500" : ""
+                }`}
+                placeholder=" "
                 required
               />
+              <label
+                htmlFor="fullName"
+                className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+              >
+                Full Name
+              </label>
               {formErrors.fullName && (
                 <p className="error-message">{formErrors.fullName}</p>
               )}
-            </label>
-
-            <br />
-            <label>
-              Email:
+            </div>
+            <div className="relative z-0 w-full mb-5 group">
               <input
                 type="email"
+                name="email"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
+                id="email"
+                className={`block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer ${
+                  formErrors.email ? "border-red-500" : ""
+                }`}
+                placeholder=" "
                 required
               />
+              <label
+                htmlFor="email"
+                className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+              >
+                Email Address
+              </label>
               {formErrors.email && (
                 <p className="error-message">{formErrors.email}</p>
               )}
-            </label>
-
-            <br />
-            <label>
-              Available Slots:
-              <select
-                value={selectedSlot}
-                onChange={(e) => setSelectedSlot(e.target.value)}
-                required
-              >
-                <option value="default">Select</option>
-              </select>
-              {formErrors.selectedSlot && (
-                <p className="error-message">{formErrors.selectedSlot}</p>
-              )}
-            </label>
-
-            <br />
-            <div className="flex justify-center">
-              <button type="submit" className="text-appointment-btn font-bold">
-                Confirm Appointment
-              </button>
             </div>
-
-            <p
-              className="success-message"
-              style={{ display: isSubmitted ? "block" : "none" }}
+          </div>
+          <div className="relative z-0 w-full group">
+            <label
+              htmlFor="slots"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
             >
-              Appointment details have been sent to the patient's email.
-            </p>
-          </form>
-        </div>
+              Available Slots
+            </label>
+            <select
+              id="slots"
+              value={selectedSlot}
+              onChange={(e) => setSelectedSlot(e.target.value)}
+              className={`bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 ${
+                formErrors.selectedSlot ? "border-red-500" : ""
+              }`}
+            >
+              <option value="default">Select</option>
+              <option value="19/5/2024">19/5/2024</option>
+              <option value="20/5/2024">20/5/2024</option>
+              <option value="21/5/2024">21/5/2024</option>
+              <option value="22/5/2024">22/5/2024</option>
+            </select>
+            {formErrors.selectedSlot && (
+              <p className="error-message">{formErrors.selectedSlot}</p>
+            )}
+          </div>
+          <button
+            type="submit"
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            Book Appointment
+          </button>
+        </form>
       </div>
-
       <ToastContainer autoClose={5000} limit={1} closeButton={false} />
     </div>
   );
 }
-
-export default AppointmentForm;

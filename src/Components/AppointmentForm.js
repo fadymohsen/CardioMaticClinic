67d059a975cbf377ad194727a94648e333e5811b
mohamed mosilhy <93820559+mohamedmosilhy@users.noreply.github.com>
2024-05-21 16:08:10 +0300

    Update AppointmentForm.js

--- src/Components/AppointmentForm.js
+++ src/Components/AppointmentForm.js
@@ -7,9 +7,8 @@ export default function AppointmentForm() {
     window.scrollTo({ top: 0, behavior: "smooth" });
   }, []);
 
-  const [fullName, setFullName] = useState("");
-  const [email, setEmail] = useState("");
   const [selectedSlot, setSelectedSlot] = useState("default");
+  const [selectedDoctor, setSelectedDoctor] = useState("default");
   const [isSubmitted, setIsSubmitted] = useState(false);
   const [formErrors, setFormErrors] = useState({});
 
@@ -18,18 +17,12 @@ export default function AppointmentForm() {
 
     // Validate form inputs
     const errors = {};
-    if (!fullName.trim()) {
-      errors.fullName = "Full name is required";
-    }
-
-    if (!email.trim()) {
-      errors.email = "Email is required";
-    } else if (!/\S+@\S+\.\S+/.test(email)) {
-      errors.email = "Invalid email format";
-    }
-
     if (selectedSlot === "default") {
       errors.selectedSlot = "Please select an available slot";
+    }
+
+    if (selectedDoctor === "default") {
+      errors.selectedDoctor = "Please select a doctor";
     }
 
     if (Object.keys(errors).length > 0) {
@@ -38,9 +31,8 @@ export default function AppointmentForm() {
     }
 
     // Reset form fields and errors after successful submission
-    setFullName("");
-    setEmail("");
     setSelectedSlot("default");
+    setSelectedDoctor("default");
     setFormErrors({});
 
     toast.success("Appointment Scheduled!", {
@@ -51,58 +43,10 @@ export default function AppointmentForm() {
   };
 
   return (
-    <div className="appointment-form-section flex mt-20 justify-center ">
+    <div className="appointment-form-section flex mt-20 justify-center">
       <div className="form-container w-5/12 p-8 border border-gray-300 rounded-lg">
         <h2 className="text-xl font-semibold mb-4">Book Appointment</h2>
         <form onSubmit={handleSubmit} className="space-y-4">
-          <div className="grid md:grid-cols-2 md:gap-6">
-            <div className="relative z-0 w-full mb-5 group">
-              <input
-                type="text"
-                name="fullName"
-                value={fullName}
-                onChange={(e) => setFullName(e.target.value)}
-                id="fullName"
-                className={`block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer ${
-                  formErrors.fullName ? "border-red-500" : ""
-                }`}
-                placeholder=" "
-                required
-              />
-              <label
-                htmlFor="fullName"
-                className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-              >
-                Full Name
-              </label>
-              {formErrors.fullName && (
-                <p className="error-message">{formErrors.fullName}</p>
-              )}
-            </div>
-            <div className="relative z-0 w-full mb-5 group">
-              <input
-                type="email"
-                name="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                id="email"
-                className={`block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer ${
-                  formErrors.email ? "border-red-500" : ""
-                }`}
-                placeholder=" "
-                required
-              />
-              <label
-                htmlFor="email"
-                className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-              >
-                Email Address
-              </label>
-              {formErrors.email && (
-                <p className="error-message">{formErrors.email}</p>
-              )}
-            </div>
-          </div>
           <div className="relative z-0 w-full group">
             <label
               htmlFor="slots"
@@ -128,6 +72,30 @@ export default function AppointmentForm() {
               <p className="error-message">{formErrors.selectedSlot}</p>
             )}
           </div>
+          <div className="relative z-0 w-full group">
+            <label
+              htmlFor="doctors"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Select Doctor
+            </label>
+            <select
+              id="doctors"
+              value={selectedDoctor}
+              onChange={(e) => setSelectedDoctor(e.target.value)}
+              className={`bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 ${
+                formErrors.selectedDoctor ? "border-red-500" : ""
+              }`}
+            >
+              <option value="default">Select</option>
+              <option value="Mohamed Sayed">Mohamed Sayed</option>
+              <option value="Sondos Mahmoud">Sondos Mahmoud</option>
+              <option value="Rana Mohamed">Rana Mohamed</option>
+            </select>
+            {formErrors.selectedDoctor && (
+              <p className="error-message">{formErrors.selectedDoctor}</p>
+            )}
+          </div>
           <button
             type="submit"
             className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
@@ -137,6 +105,7 @@ export default function AppointmentForm() {
         </form>
       </div>
       <ToastContainer autoClose={5000} limit={1} closeButton={false} />
+         
     </div>
   );
 }
